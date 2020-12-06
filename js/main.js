0f1d799cf3f750e4cd5c531c797f2614607bdbcc
Admildo Manuel <admildo68@gmail.com>
2020-12-06 15:02:19 +0100

    anew layout

--- js/main.js
+++ js/main.js
@@ -3,20 +3,20 @@ let memoji = document.getElementById('memoji');
 let count = 1;
 let count2 = 11;
 setInterval(() => {
-    me.src = `http://127.0.0.1:5500/img/me${count}.PNG`
-    if (count <= 10) {
+    me.src = `img/fme${count}.PNG`
+    if (count <= 4) {
         count++
-        if (count > 10) {
+        if (count > 4) {
             count = 1
         }
     }
-}, 1000);
+}, 2000);
 setInterval(() => {
-    memoji.src = `http://127.0.0.1:5500/img/me${count2}.PNG`
-    if (count2 <= 15) {
+    memoji.src = `img/me${count2}.PNG`
+    if (count2 <= 13) {
         count2++
-        if (count2 > 15) {
+        if (count2 > 13) {
             count2 = 11
         }
     }
-}, 1000);
+}, 2500);
