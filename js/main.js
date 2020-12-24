48bfa19a62efce5d7435132d0b5011bca7e26a6b
Admildo Manuel <admildo68@gmail.com>
2020-12-24 17:57:27 +0100

    new layout

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
 let me = document.getElementById('memo');
 let memoji = document.getElementById('memoji');
+let menu = document.getElementById('menuToogle');
+let social = document.getElementById('socail');
 let count = 1;
 let count2 = 11;
 setInterval(() => {
     me.src = `img/fme${count}.PNG`
-    if (count <= 4) {
+    if (count <= 5) {
         count++
-        if (count > 4) {
+        if (count > 5) {
             count = 1
         }
     }
 }, 2000);
+
+/*
 setInterval(() => {
     memoji.src = `img/me${count2}.PNG`
     if (count2 <= 13) {
@@ -20,3 +24,7 @@ setInterval(() => {
         }
     }
 }, 2500);
+*/
+menu.addEventListener('click', event => {
+    social.classList.toggle('hide')
+})
